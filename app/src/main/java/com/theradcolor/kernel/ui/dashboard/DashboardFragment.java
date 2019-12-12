37705b50b9ce7f8e0d1041242c96880d84aa37ac
rad <theradcolor@gmail.com>
2019-12-12 18:13:47 +0530

    app: added onSeekbar changed tracking and touch for settings vibration and continuous vibration while on seekbard.
    Signed-off-by: rad <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/theradcolor/kernel/ui/dashboard/DashboardFragment.java
@@ -1,11 +1,16 @@
 package com.theradcolor.kernel.ui.dashboard;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,7 +33,9 @@ import java.io.OutputStreamWriter;
 public class DashboardFragment extends Fragment implements View.OnClickListener{
 
     private DashboardViewModel dashboardViewModel;
-    private TextView srgbon,srgboff;
+    private TextView srgbon,srgboff,vib;
+    private SeekBar seekBar;
+    int progressChangedValue = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,6 +44,36 @@ public class DashboardFragment extends Fragment implements View.OnClickListener{
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         srgbon = root.findViewById(R.id.srgbon);
         srgboff = root.findViewById(R.id.srgboff);
+        seekBar = root.findViewById(R.id.vibration);
+        vib = root.findViewById(R.id.pervib);
+        final Vibrator v = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+                vib.setText(progress + "%");Vibrator v = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                 // Vibrate for 500 milliseconds
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    long[] pattern = {1, 1000, 0};
+                    v.vibrate(VibrationEffect.createWaveform(pattern,1));
+                } else {
+                    //deprecated in API 26
+                    v.vibrate(500);
+                }
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getContext(), "Seek bar progress is :" + progressChangedValue,
+                        Toast.LENGTH_SHORT).show();
+                v.cancel();
+            }
+        });
         srgbon.setOnClickListener(this);
         srgboff.setOnClickListener(this);
         //final TextView textView = root.findViewById(R.id.text_dashboard);
