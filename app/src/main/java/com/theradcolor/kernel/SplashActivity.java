2500199717bac41e93d913402fc61babd57e8a8e
rad <theradcolor@gmail.com>
2019-12-28 21:50:53 +0530

    app: theming Splash
    Signed-off-by: rad <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/SplashActivity.java
+++ app/src/main/java/com/theradcolor/kernel/SplashActivity.java
@@ -3,15 +3,22 @@ package com.theradcolor.kernel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            }
+        },1000);
 
     }
 }
