9d66255820c0f81ffa9c510ffef62387026a5f00
rad <theradcolor@gmail.com>
2019-12-29 17:36:27 +0530

    app: tweaks: adding func
    Signed-off-by: rad <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/ui/Misc/MiscFragment.java
+++ app/src/main/java/com/theradcolor/kernel/ui/Misc/MiscFragment.java
@@ -23,7 +23,7 @@ import com.theradcolor.kernel.R;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class MiscFragment extends Fragment {
+public class MiscFragment extends Fragment implements View.OnClickListener{
 
     private MiscViewModel dashboardViewModel;
     private TextView srgbon,srgboff,vib;
@@ -36,5 +36,75 @@ public class MiscFragment extends Fragment {
                 ViewModelProviders.of(this).get(MiscViewModel.class);
         View root = inflater.inflate(R.layout.fragment_misc, container, false);
         return root;
+    }
+
+    private void execCommandLine(String command)
+    {
+        Runtime runtime = Runtime.getRuntime();
+        Process proc = null;
+        OutputStreamWriter osw = null;
+
+        try
+        {
+            proc = runtime.exec("su");
+            osw = new OutputStreamWriter(proc.getOutputStream());
+            osw.write(command);
+            osw.flush();
+            osw.close();
+        }
+        catch (IOException ex)
+        {
+            Log.e("execCommandLine()", "Command resulted in an IO Exception: " + command);
+            return;
+        }
+        finally
+        {
+            if (osw != null)
+            {
+                try
+                {
+                    osw.close();
+                }
+                catch (IOException e){}
+            }
+        }
+
+        try
+        {
+            proc.waitFor();
+        }
+        catch (InterruptedException e){}
+
+        if (proc.exitValue() != 0)
+        {
+            Log.e("execCommandLine()", "Command returned error: " + command + "\n  Exit code: " + proc.exitValue());
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int view = v.getId();
+        switch (view) {
+            /*case R.id.srgbon:
+                execCommandLine("active=1\n" +
+                        "\n" +
+                        "echo $active > /sys/module/mdss_fb/parameters/srgb_enabled\n" +
+                        "\n" +
+                        "if [ $active = \"1\" ]\n" +
+                        "then echo \"2\" > /sys/class/graphics/fb0/msm_fb_srgb\n" +
+                        "else echo \"1\" > /sys/class/graphics/fb0/msm_fb_srgb\n" +
+                        "fi");
+                break;
+            case R.id.srgboff:
+                execCommandLine("active=0\n" +
+                        "\n" +
+                        "echo $active > /sys/module/mdss_fb/parameters/srgb_enabled\n" +
+                        "\n" +
+                        "if [ $active = \"1\" ]\n" +
+                        "then echo \"2\" > /sys/class/graphics/fb0/msm_fb_srgb\n" +
+                        "else echo \"1\" > /sys/class/graphics/fb0/msm_fb_srgb\n" +
+                        "fi");
+                break;*/
+        }
     }
 }
