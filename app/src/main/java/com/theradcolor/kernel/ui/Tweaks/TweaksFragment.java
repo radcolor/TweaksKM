eb86115cf2049103203411803a48e2080ad08b16
Shashank Baghel <theradcolor@gmail.com>
2020-06-13 16:13:57 +0530

    monitor: do fixed timer interval of 1000ms to update cpu freq
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/ui/Tweaks/TweaksFragment.java
+++ app/src/main/java/com/theradcolor/kernel/ui/Tweaks/TweaksFragment.java
@@ -2,11 +2,13 @@ package com.theradcolor.kernel.ui.Tweaks;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,11 +16,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.grarak.kerneladiutor.utils.root.RootUtils;
 import com.theradcolor.kernel.R;
+import com.theradcolor.utils.kernel.CPU;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TweaksFragment extends Fragment implements View.OnClickListener{
 
     private TweaksViewModel homeViewModel;
+    CPU cpu;
     public TextView textView;
+    private TextView cpu0,cpu1,cpu2,cpu3,cpu4,cpu5,cpu6,cpu7;
     SharedPreferences preferences;
 
     public View onCreateView(@NonNull final LayoutInflater inflater,
@@ -28,7 +36,18 @@ public class TweaksFragment extends Fragment implements View.OnClickListener{
         textView = root.findViewById(R.id.kernel_name);
         textView.setText("Kernel: " + RootUtils.runCommand("uname -a"));
 
+        cpu = new CPU();
+        cpu0 = root.findViewById(R.id.cpu0);
+        cpu1 = root.findViewById(R.id.cpu1);
+        cpu2 = root.findViewById(R.id.cpu2);
+        cpu3 = root.findViewById(R.id.cpu3);
+        cpu4 = root.findViewById(R.id.cpu4);
+        cpu5 = root.findViewById(R.id.cpu5);
+        cpu6 = root.findViewById(R.id.cpu6);
+        cpu7 = root.findViewById(R.id.cpu7);
+
         preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        refreshfreq();
         return root;
     }
 
@@ -38,4 +57,23 @@ public class TweaksFragment extends Fragment implements View.OnClickListener{
         switch (view) {
         }
     }
+
+    public void refreshfreq(){
+        //Declare the timer
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                cpu0.setText(cpu.getCurFreq(0)/1000+"MHz");
+                cpu1.setText(cpu.getCurFreq(1)/1000+"MHz");
+                cpu2.setText(cpu.getCurFreq(2)/1000+"MHz");
+                cpu3.setText(cpu.getCurFreq(3)/1000+"MHz");
+                cpu4.setText(cpu.getCurFreq(4)/1000+"MHz");
+                cpu5.setText(cpu.getCurFreq(5)/1000+"MHz");
+                cpu6.setText(cpu.getCurFreq(6)/1000+"MHz");
+                cpu7.setText(cpu.getCurFreq(7)/1000+"MHz");
+            }
+            }, 0, 1000);
+    }
+
 }
---
+++ app/src/main/java/com/theradcolor/utils/kernel/CPU.java
@@ -0,0 +1,23 @@
+package com.theradcolor.utils.kernel;
+
+import com.grarak.kerneladiutor.utils.Utils;
+
+public class CPU {
+
+    private static final String CUR_FREQ = "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq";
+    private static final String AVAILABLE_FREQS = "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_available_frequencies";
+    private static final String CPU_MAX_FREQ = "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_max_freq";
+    private static final String CPU_MIN_FREQ = "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_min_freq";
+    public static final String CPU_ONLINE = "/sys/devices/system/cpu/cpu%d/online";
+    private static final String CPU_SCALING_GOVERNOR = "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor";
+    private static final String CPU_AVAILABLE_GOVERNORS = "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_available_governors";
+
+
+    public int getCurFreq(int cpu) {
+        String value = Utils.readFile(Utils.strFormat(CUR_FREQ, cpu));
+        if (value != null) {
+            return Utils.strToInt(value);
+        }
+        return 0;
+    }
+}
