60fdba26d2898215dc7ab04755bc406994496b1d
Shashank Baghel <theradcolor@gmail.com>
2020-07-06 14:42:41 +0530

    app: some ui views arrangements
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/activities/FAQsActivity.java
+++ app/src/main/java/com/theradcolor/kernel/activities/FAQsActivity.java
@@ -1,15 +1,8 @@
 package com.theradcolor.kernel.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-
 import com.theradcolor.kernel.R;
-
-import static android.view.View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
-import static android.view.WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS;
 
 public class FAQsActivity extends AppCompatActivity {
 
@@ -17,10 +10,6 @@ public class FAQsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faqs);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS |
-                    SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
-        }
+
     }
 }
