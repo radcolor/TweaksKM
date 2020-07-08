05cd7cc43b56b2428f304bad74f1143c350b1e9d
Shashank Baghel <theradcolor@gmail.com>
2020-07-08 10:46:36 +0530

    cpu/gpu: Acivity: disable live XY graph for now
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/activities/cpuActivity.java
+++ app/src/main/java/com/theradcolor/kernel/activities/cpuActivity.java
@@ -3,49 +3,24 @@ package com.theradcolor.kernel.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.theradcolor.kernel.R;
 import com.theradcolor.kernel.utils.kernel.cpu.CPU;
 
-import java.util.ArrayList;
-import java.util.List;
-import lecho.lib.hellocharts.gesture.ContainerScrollType;
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.model.Viewport;
-import lecho.lib.hellocharts.view.LineChartView;
-
 @SuppressLint("SetTextI18n")
 public class cpuActivity extends AppCompatActivity {
 
-    LineChartView mChart, nChart;
-    List<PointValue> bigValues = new ArrayList<>();
-    List<PointValue> littleValues = new ArrayList<>();
-    int maxNumberOfPoints = 8;
-
-    TextView bigUsage, littleUsage;
-    TextView bigGov, bigMinFreq, bigMaxFreq, litGov, litMinFreq, litMaxFreq;
     CPU cpu;
-    float[] mCPUUsages;
-    boolean[] mCPUStates;
-    int[] mCPUFreqs;
+    TextView bigGov, bigMinFreq, bigMaxFreq, litGov, litMinFreq, litMaxFreq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cpu);
-        mChart = (LineChartView) findViewById(R.id.bigCpu);
-        nChart = (LineChartView) findViewById(R.id.littleCpu);
 
         cpu = new CPU();
-        bigUsage = findViewById(R.id.big_cpu_usage);
-        littleUsage = findViewById(R.id.little_cpu_usage);
         bigGov = findViewById(R.id.tv_cpu_big_gov);
         bigMinFreq = findViewById(R.id.tv_cpu_big_min_freq);
         bigMaxFreq = findViewById(R.id.tv_cpu_big_max_freq);
@@ -53,44 +28,21 @@ public class cpuActivity extends AppCompatActivity {
         litMinFreq = findViewById(R.id.tv_cpu_lit_min_freq);
         litMaxFreq = findViewById(R.id.tv_cpu_lit_max_freq);
 
-        mChart.setInteractive(true);
-        mChart.setZoomEnabled(false);
-        mChart.setContainerScrollEnabled(false, ContainerScrollType.HORIZONTAL);
+        refreshUI();
 
-        nChart.setInteractive(true);
-        nChart.setZoomEnabled(false);
-        nChart.setContainerScrollEnabled(false, ContainerScrollType.HORIZONTAL);
-
-        List<Line> lines = new ArrayList<>();
-        Line line = new Line();
-        line.setHasLines(true);
-        line.setHasPoints(false);
-        line.setColor(getResources().getColor(R.color.colorAccent));
-        line.setFilled(true);
-
-        lines.add(line);
-        LineChartData data = new LineChartData(lines);
-        data.setAxisXBottom(null);
-        data.setAxisYLeft(null);
-        data.setBaseValue(Float.NEGATIVE_INFINITY);
-        mChart.setLineChartData(data);
-        nChart.setLineChartData(data);
-        uiInit();
-        bigGraph();
-        littleGraph();
     }
 
-    public void uiInit() {
+    public void refreshUI() {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 bigGov.setText(cpu.getGovernor(cpu.getBigCpu(), true));
-                bigMinFreq.setText(cpu.getMinFreq( true) / 1000 +getResources().getString(R.string.mhz));
-                bigMaxFreq.setText(cpu.getMaxFreq(true) / 1000 +getResources().getString(R.string.mhz));
+                bigMinFreq.setText(cpu.getMinFreq(true) / 1000 + getResources().getString(R.string.mhz));
+                bigMaxFreq.setText(cpu.getMaxFreq(true) / 1000 + getResources().getString(R.string.mhz));
 
                 litGov.setText(cpu.getGovernor(cpu.getLITTLECpu(), true));
-                litMinFreq.setText(cpu.getMinFreq(cpu.getLITTLECpu(), true) / 1000 +getResources().getString(R.string.mhz));
-                litMaxFreq.setText(cpu.getMaxFreq(cpu.getLITTLECpu(), true) / 1000 +getResources().getString(R.string.mhz));
+                litMinFreq.setText(cpu.getMinFreq(cpu.getLITTLECpu(), true) / 1000 + getResources().getString(R.string.mhz));
+                litMaxFreq.setText(cpu.getMaxFreq(cpu.getLITTLECpu(), true) / 1000 + getResources().getString(R.string.mhz));
             }
         }).start();
     }
@@ -100,7 +52,7 @@ public class cpuActivity extends AppCompatActivity {
         super.onResume();
     }
 
-    private void bigGraph() {
+    /*private void bigGraph() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -203,5 +155,7 @@ public class cpuActivity extends AppCompatActivity {
         graph = Math.round(average);
         return graph;
     }
+
+}*/
 
 }
--- app/src/main/java/com/theradcolor/kernel/activities/gpuActivity.java
+++ app/src/main/java/com/theradcolor/kernel/activities/gpuActivity.java
@@ -5,36 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.grarak.kerneladiutor.utils.Utils;
 import com.theradcolor.kernel.R;
 import com.theradcolor.kernel.utils.kernel.GPU;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import lecho.lib.hellocharts.gesture.ContainerScrollType;
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.model.Viewport;
-import lecho.lib.hellocharts.view.LineChartView;
 
 public class gpuActivity extends AppCompatActivity implements View.OnClickListener{
 
     LinearLayout governor;
-    TextView usage, curr_freq, gov, min_freq, max_freq, pwr_lvl;
-    LineChartView mChart;
-    List<PointValue> pointValues = new ArrayList<>();
-    int maxNumberOfPoints = 16;
+    TextView gov, min_freq, max_freq, pwr_lvl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpu);
-        mChart = (LineChartView) findViewById(R.id.gpu_chart);
-        usage = findViewById(R.id.gpu_usage);
-        curr_freq = findViewById(R.id.curr_freq);
         gov = findViewById(R.id.tv_gpu_gov);
         min_freq = findViewById(R.id.tv_gpu_min_freq);
         max_freq = findViewById(R.id.tv_gpu_max_freq);
@@ -43,29 +25,10 @@ public class gpuActivity extends AppCompatActivity implements View.OnClickListen
         governor = findViewById(R.id.ll_gpu_gov);
         governor.setOnClickListener(this);
 
-        mChart.setInteractive(true);
-        mChart.setZoomEnabled(false);
-        mChart.setContainerScrollEnabled(false, ContainerScrollType.HORIZONTAL);
-
-        List<Line> lines = new ArrayList<>();
-        Line line = new Line();
-        line.setHasLines(true);
-        line.setHasPoints(false);
-        line.setColor(getResources().getColor(R.color.colorAccent));
-        line.setFilled(true);
-
-        lines.add(line);
-        LineChartData data = new LineChartData(lines);
-        data.setAxisXBottom(null);
-        data.setAxisYLeft(null);
-        data.setBaseValue(Float.NEGATIVE_INFINITY);
-        mChart.setLineChartData(data);
-
-        drawGraph();
         refreshUI();
     }
 
-    private void drawGraph() {
+    /*private void drawGraph() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -95,13 +58,18 @@ public class gpuActivity extends AppCompatActivity implements View.OnClickListen
             viewport.left = size - 16;
             mChart.setCurrentViewport(viewport);
         }
-    }
+    }*/
 
-    private void refreshUI(){
-        gov.setText(GPU.getGoverner());
-        min_freq.setText(GPU.getMinFreq()/1000000+getResources().getString(R.string.mhz));
-        max_freq.setText(GPU.getMaxFreq()/1000000+getResources().getString(R.string.mhz));
-        pwr_lvl.setText(GPU.getPwrLevel());
+    public void refreshUI() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                gov.setText(GPU.getGoverner());
+                min_freq.setText(GPU.getMinFreq()/1000000+getResources().getString(R.string.mhz));
+                max_freq.setText(GPU.getMaxFreq()/1000000+getResources().getString(R.string.mhz));
+                pwr_lvl.setText(GPU.getPwrLevel());
+            }
+        }).start();
     }
 
     @Override
