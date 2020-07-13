a427f6d7ef0e2f47abbecebfe467cdb06c32d67b
Shashank Baghel <theradcolor@gmail.com>
2020-07-14 01:57:18 +0530

    cpuActivity: migrate to kotlin coroutines from AsyncTask for XYGraph
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/activities/cpuActivity.kt
+++ app/src/main/java/com/theradcolor/kernel/activities/cpuActivity.kt
@@ -2,8 +2,7 @@ package com.theradcolor.kernel.activities
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_cpu.*
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ContextThemeWrapper
@@ -11,144 +10,189 @@ import com.grarak.kerneladiutor.utils.Prefs
 import com.theradcolor.kernel.R
 import com.theradcolor.kernel.utils.kernel.GPU
 import com.theradcolor.kernel.utils.kernel.cpu.CPU
+import kotlinx.android.synthetic.main.activity_cpu.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import lecho.lib.hellocharts.gesture.ContainerScrollType
+import lecho.lib.hellocharts.model.Line
+import lecho.lib.hellocharts.model.LineChartData
+import lecho.lib.hellocharts.model.PointValue
+import lecho.lib.hellocharts.model.Viewport
+import kotlin.math.roundToInt
 
 @SuppressLint("SetTextI18n")
 class cpuActivity : AppCompatActivity() {
 
-    var cpu: CPU? = null
+    lateinit var cpu: CPU
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cpu)
         cpu = CPU()
+        initGraph()
         refreshUI()
         ll_cpu_lit_gov.setOnClickListener {  govDialog("little") }
         ll_cpu_lit_min.setOnClickListener {  minDialog("little") }
+        CoroutineScope(Dispatchers.Default).launch{ readBigData() }
+        CoroutineScope(Dispatchers.Default).launch { readLittleData() }
     }
 
     private fun refreshUI() {
-        tv_cpu_big_gov.text = cpu!!.getGovernor(cpu!!.bigCpu, true)
-        tv_cpu_big_min_freq.text = cpu!!.getMinFreq(4, true).div(1000).toString().plus(resources.getString(R.string.mhz))
-        tv_cpu_big_max_freq.text = cpu!!.getMaxFreq(4,true).div(1000).toString().plus(resources.getString(R.string.mhz))
-        tv_cpu_lit_gov.text = cpu!!.getGovernor(0, true)
-        tv_cpu_lit_min_freq.text = cpu!!.getMinFreq(0, true).div(1000).toString().plus(resources.getString(R.string.mhz))
-        tv_cpu_lit_max_freq.text = cpu!!.getMaxFreq(0, true).div(1000).toString().plus(resources.getString(R.string.mhz))
+        tv_cpu_big_gov.text = cpu.getGovernor(4, true)
+        tv_cpu_big_min_freq.text = cpu.getMinFreq(4, true).div(1000).toString().plus(resources.getString(R.string.mhz))
+        tv_cpu_big_max_freq.text = cpu.getMaxFreq(4,true).div(1000).toString().plus(resources.getString(R.string.mhz))
+        tv_cpu_lit_gov.text = cpu.getGovernor(0, true)
+        tv_cpu_lit_min_freq.text = cpu.getMinFreq(0, true).div(1000).toString().plus(resources.getString(R.string.mhz))
+        tv_cpu_lit_max_freq.text = cpu.getMaxFreq(0, true).div(1000).toString().plus(resources.getString(R.string.mhz))
     }
 
     override fun onResume() {
         super.onResume()
-        refreshUI()
     }
 
-    /*private void bigGraph() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 999; ++i) {
-                    try {
-                        mCPUUsages = cpu.getCpuUsage();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    mCPUStates = new boolean[cpu.getCpuCount()];
-                    for (int s = 0; s < mCPUStates.length; s++) {
-                        mCPUStates[s] = !cpu.isOffline(s);
-                    }
-                    mCPUFreqs = new int[cpu.getCpuCount()];
-                    for (int c = 0; c < mCPUFreqs.length; c++) {
-                        mCPUFreqs[c] = cpu.getCurFreq(c);
-                    }
+    private lateinit var mCPUUsages:FloatArray
+    private lateinit var mCPUStates:BooleanArray
+    private lateinit var mCPUFreq:IntArray
 
-                    LineChartData bigData = mChart.getLineChartData();
-                    bigValues.add(new PointValue(i, refreshUsages(mCPUUsages, cpu.getBigCpuRange(), mCPUStates)));
+    private var bigValues: MutableList<PointValue> = ArrayList()
+    private var littleValues: MutableList<PointValue> = ArrayList()
+    private var maxNumberOfPoints = 8
 
-                    Log.d("CPU B", ""+refreshUsages(mCPUUsages, cpu.getBigCpuRange(), mCPUStates));
-                    bigUsage.setText((int)refreshUsages(mCPUUsages, cpu.getBigCpuRange(), mCPUStates)
-                            +getResources().getString(R.string.percent));
-
-                    bigData.getLines().get(0).setValues(new ArrayList<>(bigValues));
-
-                    mChart.setLineChartData(bigData);
-                    bigViewPort();
-                }
+    private suspend fun readBigData(){
+        for (i in 0..998)
+        {
+            delay(500)
+            try
+            {
+                mCPUUsages = cpu.cpuUsage
             }
-        }).start();
-    }
-
-    private void littleGraph() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 999; ++i) {
-                    try {
-                        mCPUUsages = cpu.getCpuUsage();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    mCPUStates = new boolean[cpu.getCpuCount()];
-                    for (int s = 0; s < mCPUStates.length; s++) {
-                        mCPUStates[s] = !cpu.isOffline(s);
-                    }
-                    mCPUFreqs = new int[cpu.getCpuCount()];
-                    for (int c = 0; c < mCPUFreqs.length; c++) {
-                        mCPUFreqs[c] = cpu.getCurFreq(c);
-                    }
-                    LineChartData littleData = nChart.getLineChartData();
-                    littleValues.add(new PointValue(i, refreshUsages(mCPUUsages, cpu.getLITTLECpuRange(), mCPUStates)));
-
-                    Log.d("CPU L", ""+refreshUsages(mCPUUsages, cpu.getLITTLECpuRange(), mCPUStates));
-                    littleUsage.setText((int)refreshUsages(mCPUUsages, cpu.getLITTLECpuRange(), mCPUStates)
-                            +getResources().getString(R.string.percent));
-
-                    littleData.getLines().get(0).setValues(new ArrayList<>(littleValues));
-
-                    nChart.setLineChartData(littleData);
-                    littleViewPort();
-                }
+            catch (e:InterruptedException) {
+                e.printStackTrace()
             }
-        }).start();
-    }
+            mCPUStates = BooleanArray(cpu.cpuCount)
+            for (s in mCPUStates.indices)
+            {
+                mCPUStates[s] = !cpu.isOffline(s)
+            }
+            mCPUFreq = IntArray(cpu.cpuCount)
+            for (c in mCPUFreq.indices)
+            {
+                mCPUFreq[c] = cpu.getCurFreq(c)
+            }
 
-    private void bigViewPort() {
-        int mSize = bigValues.size();
-        if (mSize > maxNumberOfPoints) {
-            final Viewport viewport1 = new Viewport(mChart.getMaximumViewport());
-            viewport1.left = mSize - 8;
-            mChart.setCurrentViewport(viewport1);
+            Log.d("CPU B", "" + refreshUsages(mCPUUsages, cpu.bigCpuRange, mCPUStates) + Thread.currentThread().name)
+            big_cpu_usage.text = (refreshUsages(mCPUUsages, cpu.bigCpuRange, mCPUStates).toString().plus(resources.getString(R.string.percent)))
+
+            val bigData = bigCpu.lineChartData
+            bigValues.add(PointValue(i.toFloat(), refreshUsages(mCPUUsages, cpu.bigCpuRange, mCPUStates)))
+            Log.d("CPU B", "" + refreshUsages(mCPUUsages, cpu.bigCpuRange, mCPUStates))
+            bigData.lines[0].values = ArrayList(bigValues)
+            bigCpu.lineChartData = bigData
+            bigViewPort()
+
         }
     }
 
-    private void littleViewPort() {
-        int nSize = littleValues.size();
-        if (nSize > maxNumberOfPoints) {
-            final Viewport viewport = new Viewport(nChart.getMaximumViewport());
-            viewport.left = nSize - 8;
-            nChart.setCurrentViewport(viewport);
+    private suspend fun readLittleData(){
+        for (i in 0..998)
+        {
+            delay(500)
+            try
+            {
+                mCPUUsages = cpu.cpuUsage
+            }
+            catch (e:InterruptedException) {
+                e.printStackTrace()
+            }
+            mCPUStates = BooleanArray(cpu.cpuCount)
+            for (s in mCPUStates.indices)
+            {
+                mCPUStates[s] = !cpu.isOffline(s)
+            }
+            mCPUFreq = IntArray(cpu.cpuCount)
+            for (c in mCPUFreq.indices)
+            {
+                mCPUFreq[c] = cpu.getCurFreq(c)
+            }
+
+            little_cpu_usage.text = (refreshUsages(mCPUUsages, cpu.littleCpuRange, mCPUStates).toString().plus(resources.getString(R.string.percent)))
+
+            val littleData = littleCpu.lineChartData
+            littleValues.add(PointValue(i.toFloat(), refreshUsages(mCPUUsages, cpu.littleCpuRange, mCPUStates)))
+            littleData.lines[0].values = ArrayList(littleValues)
+            littleCpu.lineChartData = littleData
+            littleViewPort()
+
         }
     }
 
-    private float refreshUsages(float[] usages, List<Integer> cores, boolean[] coreStates) {
-        float graph = 0;
-        float average = 0;
-        int size = 0;
-        for (int core : cores) {
-            if (core + 1 < usages.length) {
-                if (coreStates[core]) {
-                    average += usages[core + 1];
+    private fun bigViewPort() {
+        val mSize = bigValues.size
+        if (mSize > maxNumberOfPoints)
+        {
+            val viewport1 = Viewport(bigCpu.maximumViewport)
+            viewport1.left = (mSize - 8).toFloat()
+            bigCpu.currentViewport = viewport1
+        }
+    }
+
+    private fun littleViewPort() {
+        val nSize = littleValues.size
+        if (nSize > maxNumberOfPoints)
+        {
+            val viewport = Viewport(littleCpu.maximumViewport)
+            viewport.left = (nSize - 8).toFloat()
+            littleCpu.currentViewport = viewport
+        }
+    }
+
+    private fun initGraph(){
+        bigCpu.isInteractive = true
+        bigCpu.isZoomEnabled = false
+        bigCpu.setContainerScrollEnabled(false, ContainerScrollType.HORIZONTAL)
+        littleCpu.isInteractive = true
+        littleCpu.isZoomEnabled = false
+        littleCpu.setContainerScrollEnabled(false, ContainerScrollType.HORIZONTAL)
+        val lines: MutableList<Line> = ArrayList()
+        val line = Line()
+        line.setHasLines(true)
+        line.setHasPoints(false)
+        line.color = resources.getColor(R.color.colorAccent)
+        line.isFilled = true
+        lines.add(line)
+        val data = LineChartData(lines)
+        data.axisXBottom = null
+        data.axisYLeft = null
+        data.baseValue = Float.NEGATIVE_INFINITY
+        bigCpu.lineChartData = data
+        littleCpu.lineChartData = data
+    }
+
+    private suspend fun refreshUsages(usages:FloatArray, cores:List<Int>, coreStates:BooleanArray):Float {
+        var graph = 0f
+        var average = 0f
+        var size = 0
+        for (core in cores)
+        {
+            if (core + 1 < usages.size)
+            {
+                if (coreStates[core])
+                {
+                    average += usages[core + 1]
                 }
-                size++;
+                size++
             }
         }
-        average /= size;
-        graph = Math.round(average);
-        return graph;
+        average /= size.toFloat()
+        graph = average.roundToInt().toFloat()
+        return graph
     }
-
-}*/
 
     private fun govDialog(bigLittle: String) {
-        val bigCores = cpu!!.bigCpuRange
-        val LITTLECores = cpu!!.littleCpuRange
+        val bigCores = cpu.bigCpuRange
+        val LITTLECores = cpu.littleCpuRange
         val builder = AlertDialog.Builder(ContextThemeWrapper(this@cpuActivity, R.style.CustomDialogTheme))
         builder.setTitle("CPU governor")
         val GOVs = CPU.getInstance().governors.toTypedArray()
@@ -162,7 +206,6 @@ class cpuActivity : AppCompatActivity() {
                         , this@cpuActivity)
                 Prefs.saveString("cpu_big_governor", GOVs[which], this@cpuActivity)
             }
-            refreshUI()
         }
         val dialog = builder.create()
         dialog.show()
@@ -171,16 +214,16 @@ class cpuActivity : AppCompatActivity() {
     lateinit var cpu_min:Array<String>
 
     private fun minDialog(bigLittle: String) {
-        val bigCores = cpu!!.bigCpuRange
-        val LITTLECores = cpu!!.littleCpuRange
+        val bigCores = cpu.bigCpuRange
+        val LITTLECores = cpu.littleCpuRange
         val builder = AlertDialog.Builder(ContextThemeWrapper(this@cpuActivity, R.style.CustomDialogTheme))
         builder.setTitle("CPU minimum freq")
         if (bigLittle == "little") {
-            cpu_min = cpu!!.getAdjustedFreq(cpu!!.littleCpu, this).toTypedArray()
+            cpu_min = cpu!!.getAdjustedFreq(cpu.littleCpu, this).toTypedArray()
         } else if (bigLittle == "big") {
-            cpu_min = cpu!!.getAdjustedFreq(cpu!!.bigCpu, this).toTypedArray()
+            cpu_min = cpu!!.getAdjustedFreq(cpu.bigCpu, this).toTypedArray()
         }
-        builder.setItems(cpu_min) {dialog, which ->  }
+        builder.setItems(cpu_min) {dialog, which -> refreshUI() }
         val dialog = builder.create()
         dialog.show()
     }
