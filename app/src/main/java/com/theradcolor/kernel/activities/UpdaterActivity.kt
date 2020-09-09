33dfe9de338a107e7d76286816e42de3fb744c67
Shashank Baghel <theradcolor@gmail.com>
2020-09-09 13:14:12 +0530

    updater: rewrite and add a simple layout
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- app/src/main/java/com/theradcolor/kernel/activities/UpdaterActivity.kt
+++ app/src/main/java/com/theradcolor/kernel/activities/UpdaterActivity.kt
@@ -2,11 +2,16 @@ package com.theradcolor.kernel.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import com.grarak.kerneladiutor.utils.Device;
 import com.theradcolor.kernel.R
+import kotlinx.android.synthetic.main.activity_updater.*
 
 class UpdaterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_updater)
+        val kernelString = "<font color=#FFFFFF> <b> Current kernel version: </b> </font>" + Device.getKernelVersion(true);
+        linux_version.text = Html.fromHtml(kernelString)
     }
 }
